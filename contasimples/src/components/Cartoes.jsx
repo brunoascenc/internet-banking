b9262556ef4cc057cb3f9d81ddad96629ea26ff1
brunoascenc <brunoascenc1@outlook.com>
2020-10-19 22:49:32 -0300

    cartoes interface

--- contasimples/src/components/Cartoes.jsx
+++ contasimples/src/components/Cartoes.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Menu from './Menu'
+import '../App.css'
 
 export default function Cartoes() {
     return (
         <div className="home-container">
             <Menu/>
-            <h1>Cartoes</h1>        
+            <div className="cartoes-empresa">
+            <h1>Cartoes da Empresa</h1>
+                <div className="header">
+                    <span>Cartão</span>
+                    <span>Final</span>
+                </div>
+                <div className="cartoes">
+                    <h4>Nome do cartão</h4>
+                    <span>**** 1234</span>
+                </div>
+                <div className="cartoes">
+                    <h4>Nome do cartão</h4>
+                    <span>**** 1234</span>
+                </div>
+                <div className="cartoes">
+                    <h4>Nome do cartão</h4>
+                    <span>**** 1234</span>
+                </div>
+            </div>       
         </div>
     )
 }
