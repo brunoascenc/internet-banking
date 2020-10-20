c8f32cc4f65e556227ff7b12a13a154866fca489
brunoascenc <brunoascenc1@outlook.com>
2020-10-19 22:48:51 -0300

    detalhes extrato

--- contasimples/src/components/Extrato.jsx
+++ contasimples/src/components/Extrato.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
-import Menu from './Menu'
+import React from "react";
+import Menu from "./Menu";
+import "../App.css";
 
 export default function Extrato() {
-    return (
-        <div className="home-container">
-            <Menu/>
-            <h1>Extrato</h1>
+  return (
+    <div className="home-container">
+      <Menu />
+      <div className="extrato extrato-detalhe">
+        <div>
+          <h3>Extrato</h3>
         </div>
-    )
+        <div className="filter">
+          <span>Tipo de transação:</span>
+          <select>
+            <option>Cartão de crédito</option>
+            <option>Boleto</option>
+          </select>
+        </div>
+        <table>
+          <tr>
+            <th>Estabelecimento</th>
+            <th>Tipo</th>
+            <th>Valor</th>
+          </tr>
+          <tr>
+            <td>Facebook</td>
+            <td>Transferencia Bancaria</td>
+            <td>R$ 800,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>Compra com cartão</td>
+            <td>R$ 2555,00</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
